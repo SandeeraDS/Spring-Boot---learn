ead3c8869b6f9fb19c64702c1b7ec1960746067d
sandeeraDS <sandeerads@gmail.com>
2019-12-25 16:14:17 +0530

    SessionAttributes

--- 02 Web Application with Spring Boot/first_app/src/main/java/com/example/demo/controller/TodoController.java
+++ 02 Web Application with Spring Boot/first_app/src/main/java/com/example/demo/controller/TodoController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.demo.service.LoginService;
 import com.example.demo.service.TodoService;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
 	
 	@Autowired
@@ -19,24 +21,9 @@ public class TodoController {
 	
 	@RequestMapping(value ="/list-todos", method = RequestMethod.GET)
 	public String showTodosList(ModelMap model) {
-		model.put("todos", service.retrieveTodos("in28Minutes"));
-//		System.out.println("name is "+name);
+		String name = (String)model.get("name");
+		model.put("todos", service.retrieveTodos(name));
 		return "list-todos";
 	}
-//	
-//	@RequestMapping(value = "/login", method = RequestMethod.POST)
-//	public String showWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
-//		boolean isValidUser = service.validateUser(name, password);
-//		
-//		if (!isValidUser) {
-//			model.put("errorMessage","Invalid Creaditials");
-//			return "login";
-//		}
-//		
-//		model.put("name", name);
-//		model.put("password", password);
-////		System.out.println("name is "+name);
-//		return "welcome";
-//	}
-//
+
 }
--- 02 Web Application with Spring Boot/first_app/src/main/java/com/example/demo/service/TodoService.java
+++ 02 Web Application with Spring Boot/first_app/src/main/java/com/example/demo/service/TodoService.java
@@ -16,10 +16,10 @@ public class TodoService {
     private static int todoCount = 3;
 
     static {
-        todos.add(new Todo(1, "in28Minutes", "Learn Spring MVC", new Date(),
+        todos.add(new Todo(1, "154048G", "Learn Spring MVC", new Date(),
                 false));
-        todos.add(new Todo(2, "in28Minutes", "Learn Struts", new Date(), false));
-        todos.add(new Todo(3, "in28Minutes", "Learn Hibernate", new Date(),
+        todos.add(new Todo(2, "154048G", "Learn Struts", new Date(), false));
+        todos.add(new Todo(3, "154048G", "Learn Hibernate", new Date(),
                 false));
     }
 
